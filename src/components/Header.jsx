0840ddaed86340a793707e6695022b83f98597ab
Ruslan <ruslan.bulsunaev@mail.ru>
2020-07-01 17:11:05 +0500

    add new project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+
+const Header  = () => {
+      return(
+        <header className='header'>
+            <img src='https://cdn3.iconfinder.com/data/icons/social-media-1-4/36/38-256.png'/>
+        </header>
+          );
+
+
+
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import React  from 'react';
+
+const Profile = () => {
+    return (
+        <div className='Profile'>
+            <div>
+            <img src='https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80' />
+            </div>
+            <div className='Ava'>
+                ava+ описание
+            </div>
+            <div className='My-Post'>
+                posts
+                <div className='New-post'>
+                    new post
+                </div>
+                <div>
+                    <div className='one post'>
+                        1 post
+                    </div>
+                    <div className='two post'>
+                        2 post
+                    </div>
+                </div>
+            </div>
+        </div>);
+
+        }
+        export default Profile;
+
+
+
+
+
